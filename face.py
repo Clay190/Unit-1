c2fc9ce39143491f32bd338252509a88b06635b1
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-08-31 14:30:50 -0400

    Updated from Brython Server: 8/31/2017 2:30:49 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Clay Kynor
+#8/31/17
+#face.py - Make a face problem
 
+print ("   XXXXX   ")
+print ("  X     X  ")
+print ("((  o o  ))")
+print ("  |  V  |  ")
+print ("  | === |  ")
+print ("   -----   ")
