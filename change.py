82a32f056ae57d3303fa53046640bbaf06fd5bcd
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-07 14:44:09 -0400

    Updated from Brython Server: 9/7/2017 2:44:08 PM

--- change.py
+++ change.py
@@ -1,1 +1,13 @@
+#Clay Kynor
+#9/7/17
+#change.py calculates change
 
+cents = int(input("Input a number of cents: "))
+quarters = (cents//25)
+dimes = ((cents-(quarters*25))//10)
+nickels = ((cents-(quarters*25)-(dimes*10))//5)
+pennies = (cents-(quarters*25)-(dimes*10))-(nickels*5)
+print("Quarters:",quarters)
+print("Dimes:",dimes)
+print("Nickels:",nickels)
+print("Pennies:",pennies)
