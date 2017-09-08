8937054af8f1c851148e59a415a2187f9e09fbf6
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-08 14:39:15 -0400

    Updated from Brython Server: 9/8/2017 2:39:14 PM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,3 +1,11 @@
 #Clay Kynor
 #9/8/17
 #isItATriangle.py - finding wether or not three sides could create a triangle
+
+sideA = int(input("Enter side A: ",))
+sideB = int(input("Enter side B: ",))
+sideC = int(input("Enter side C: ",))
+maximum = max(sideA,sideB,sideC)
+minimum = min(sideA,sideB,sideC)
+middle = (sideA+sideB+sideC)-maximum-minimum
+print(maximum < (middle+minimum))
