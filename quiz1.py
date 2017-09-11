315b6d9b501df05702780bb905e23e8cf492d8d7
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-11 14:21:02 -0400

    Updated from Brython Server: 9/11/2017 2:21:02 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,12 @@
+#Clay Kynor
+#9/11/17
+#quiz1.py - my quiz
 
+from random import randint
+
+print("Clay Kynor")
+number1 = int(input("Enter your first number "))
+number2 = int(input("Enter your second number "))
+print("The sum is", number1 + number2)
+print("The product is", number1 * number2)
+print("Your lucky number is", randint (1,1000))
